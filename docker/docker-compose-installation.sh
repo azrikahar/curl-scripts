22bd5131fc6d45984cf2d032054a445319fe1bbd
azrikahar <42867097+azrikahar@users.noreply.github.com>
2020-03-25 23:22:32 +0800

    fix variable assignment spacing

--- docker/docker-compose-installation.sh
+++ docker/docker-compose-installation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-latest_version = $(curl -s https://github.com/docker/compose/releases/latest | awk -F[\"\"] '{print $2}' | cut -d'/' -f8)
+latest_version=$(curl -s https://github.com/docker/compose/releases/latest | awk -F[\"\"] '{print $2}' | cut -d'/' -f8)
 sudo curl -L https://github.com/docker/compose/releases/download/$latest_version/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && 
 sudo chmod +x /usr/local/bin/docker-compose
