a8d6323386a92cb1e70ee0dfe1f4ce8fcc436f3c
azrikahar <42867097+azrikahar@users.noreply.github.com>
2020-03-25 23:08:29 +0800

    use variable in docker-compose-installation.sh

--- scripts/docker/docker-compose-installation.sh
+++ scripts/docker/docker-compose-installation.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo curl -L https://github.com/docker/compose/releases/download/$(curl -s https://github.com/docker/compose/releases/latest | awk -F[\"\"] '{print $2}' | cut -d'/' -f8)/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && 
+latest_version = $(curl -s https://github.com/docker/compose/releases/latest | awk -F[\"\"] '{print $2}' | cut -d'/' -f8)
+sudo curl -L https://github.com/docker/compose/releases/download/$latest_version/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && 
 sudo chmod +x /usr/local/bin/docker-compose
