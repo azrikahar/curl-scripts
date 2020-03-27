5d12bcd89c2594b317a383dab61d3fe341177a2e
azrikahar <42867097+azrikahar@users.noreply.github.com>
2020-03-27 22:47:38 +0800

    add braces to docker ce installation

--- docker/docker-ce-installation.sh
+++ docker/docker-ce-installation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -fsSL https://get.docker.com/ | sh && 
+{curl -fsSL https://get.docker.com/ | sh} && 
 sudo systemctl start docker &&
 sudo systemctl enable docker &&
 sudo usermod -aG docker $(whoami) &&
