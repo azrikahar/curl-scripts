24830196d2f18e9befac32668c54f8fba6a60d5a
azrikahar <42867097+azrikahar@users.noreply.github.com>
2020-03-28 20:32:26 +0800

    add verbose messages to docker scripts

--- docker/docker-ce-installation.sh
+++ docker/docker-ce-installation.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+current_user=$(whoami)
+echo "Installing Docker CE..." && \
 curl -fsSL https://get.docker.com/ | sh && \
+echo "Starting Docker daemon..." && \
 sudo systemctl start docker && \
+echo "Making sure Docker daemon starts on server reboot..." && \
 sudo systemctl enable docker && \
-sudo usermod -aG docker $(whoami) && \
+echo "Adding user $current_user to Docker group to run docker cli without sudo..." && \
+sudo usermod -aG docker  && \
+echo "Applying changes..." && \
 newgrp docker && \
 echo "Docker CE installed successfully."
--- docker/docker-compose-installation.sh
+++ docker/docker-compose-installation.sh
@@ -2,5 +2,7 @@
 latest_version=$(curl -s https://github.com/docker/compose/releases/latest | awk -F[\"\"] '{print $2}' | cut -d'/' -f8)
 echo "Installing docker-compose v$latest_version..." && \
 sudo curl -L https://github.com/docker/compose/releases/download/$latest_version/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && \
+echo "granting execute permission to docker-compose..." && \
 sudo chmod +x /usr/local/bin/docker-compose && \
-echo "docker-compose installed successfully."
+echo "docker-compose installed successfully." && \
+docker-compose --version
